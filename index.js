52063c5ec187c14b68e73f99b80ff7cece86f661
K5709 <r.kristian40@gmail.com>
2022-08-24 15:13:43 -0500

    commited after finishing task 4

--- index.js
+++ index.js
@@ -189,12 +189,31 @@ Use the game function below to do the following:
 
 RULES OF THE GAME: Scissors beats Paper | Paper beats Rock | Rock beats Scissors | Or there's a tie
 */
+let computer = Math.random();
 
-function game(user, computer){
-  /*add your code here*/
+if (computer <= 0.34){
+  computer = 'rock';
+}else if(computer <= 0.67){
+  computer = 'paper';
+}else if(computer > 0.67){
+  computer = 'scissors';
 }
 
+function game(user, computer){
+  if (user === computer){
+    return `it's a tie`;
+  }else if (user === 'rock' && computer === 'scissors'){
+    return `you win!`;
+  }else if (user === 'paper' && computer === 'rock'){
+    return `you win!`
+  }else if (user === 'scissors' && computer === 'paper'){
+    return `you win!`;
+  }else{
+    return `you lose!`;
+  }
+}
 
+console.log ('task 4', game('paper', 'computer'));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
